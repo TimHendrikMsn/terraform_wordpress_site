fe994dbaca96409ecd583c4f59744a3aa0135746
Tim <tim.hps.maassen@gmail.com>
2025-11-01 11:38:06 +0100

    formatting

--- terraform/dev/compute/main.tf
+++ terraform/dev/compute/main.tf
@@ -1,14 +1,14 @@
 module "launch_template" {
-  source = "../../modules/launch_template"
+  source                              = "../../modules/launch_template"
   name_prefix                         = var.name_prefix
   env                                 = var.env
   launch_template_name                = "${var.env}-${var.name_prefix}-launch-template"
   launch_template_version_description = var.launch_template_version_description
 
-  instance_type                       = var.instance_type
-  image_id                            = data.aws_ssm_parameter.linux_ami.value
-  vpc_security_group_ids              = [data.terraform_remote_state.network.outputs.sg_entrypoint_id]
-  instance_profile                    = data.terraform_remote_state.iam.outputs.wordpress_instance_profile
+  instance_type          = var.instance_type
+  image_id               = data.aws_ssm_parameter.linux_ami.value
+  vpc_security_group_ids = [data.terraform_remote_state.network.outputs.sg_entrypoint_id]
+  instance_profile       = data.terraform_remote_state.iam.outputs.wordpress_instance_profile
   user_data = base64encode(templatefile("${path.module}/user_data.sh", {
     ENV         = var.env
     NAME_PREFIX = var.name_prefix
@@ -17,9 +17,9 @@ module "launch_template" {
 }
 
 module "asg" {
-  source = "../../modules/auto_scaling_group"
-  name_prefix                         = var.name_prefix
-  env                                 = var.env
+  source      = "../../modules/auto_scaling_group"
+  name_prefix = var.name_prefix
+  env         = var.env
 
   subnet_ids = [
     data.terraform_remote_state.network.outputs.pub_a_id,
@@ -29,9 +29,9 @@ module "asg" {
 
   alb_target_group = [data.terraform_remote_state.network.outputs.alb_tg_arn]
   capacity_desired = 1
-  capacity_min = 1
-  capacity_max = 3
-  
+  capacity_min     = 1
+  capacity_max     = 3
+
   launch_template_id      = module.launch_template.launch_template_id
   launch_template_version = module.launch_template.launch_template_latest_version
 }
